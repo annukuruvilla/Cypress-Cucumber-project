a94ea07fe30cefb5005e3f99fd5ca1b32c2e2389
ANNU KURUVILLA <annukuruvilla25@gmail.com>
2025-02-27 12:46:10 +0400

    Update add_to_cart.js

--- cypress/support/step_definitions/add_to_cart.js
+++ cypress/support/step_definitions/add_to_cart.js
@@ -11,9 +11,13 @@ When("I search for {string}", (productName) => {
 
 When("I add the first product to the cart", () => {
   cy.get(".productinfo .btn").first().click();
+  cy.get("#cartModal").should("be.visible");
+  //cy.get("a[href='/view_cart']").click();
+  cy.get("#cartModal").find("a[href='/view_cart']").click();
 });
 
 Then("I should see the product in the shopping cart", () => {
-  cy.visit("https://automationexercise.com/view_cart");
-  cy.get("#cart_items").should("be.visible");
+  //cy.visit("https://automationexercise.com/view_cart");
+  cy.url().should("include", "/view_cart");
+  //cy.get("#cart_items").should("be.visible");
 });
